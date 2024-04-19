4171b43bac684b0188cdee34d1b63b3652cc560e
Tom Creuse <tom.creuse@gmail.com>
2024-04-19 21:35:14 +0200

    modifications on "about" page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,40 +1,41 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { ImPointRight } from 'react-icons/im';
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className='quote-card-view'>
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+        <blockquote className='blockquote mb-0'>
+          <p style={{ textAlign: 'justify' }}>
+            Hi Everyone, I am <span className='purple'> Tom Creuse </span>
+            from <span className='purple'> Vincennes, France.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently working at employed in the digital department of the
+            french Ministry of the Economy and Finance.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have a Master's degree in law and a diploma in web development at
+            OpenClassrooms.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
-            <li className="about-activity">
+            <li className='about-activity'>
               <ImPointRight /> Playing Games
             </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+            <li className='about-activity'>
+              <ImPointRight /> Playing the piano
             </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
+            <li className='about-activity'>
+              <ImPointRight /> Sports : running, boxing, judo, ...
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          <p style={{ color: 'rgb(155 126 172)' }}>
+            "The change you want to see in the world starts with yourself."{' '}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className='blockquote-footer'>Mahatma Gandhi</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,18 +1,18 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+import { Row } from 'react-bootstrap';
 
 function Github() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
+    <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+      <h1 className='project-heading' style={{ paddingBottom: '20px' }}>
+        Days I <strong className='purple'>Code</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username='tomcre94'
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color='#c084f5'
         fontSize={16}
       />
     </Row>
